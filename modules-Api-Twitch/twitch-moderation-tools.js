405d1dc1ac5172ca2fea4c9d1e4df3d5166ab992
if_vitor <127361991+Vitor-VX@users.noreply.github.com>
2023-09-19 23:58:14 -0400

    Update twitch-moderation-tools.js
    Atualizando para o Módulo Axios

--- modules-Api-Twitch/twitch-moderation-tools.js
+++ modules-Api-Twitch/twitch-moderation-tools.js
@@ -3,10 +3,10 @@
 
     TODAS AS INFOS SÃO BASEADAS NA API OFICIAL DA TWTICH -> https://dev.twitch.tv/docs/api
 */
-const request = require('request');
-const POST = 'POST', GET = 'GET', DELETE = 'DELETE', PATCH = 'PATCH';
+const axios = require('axios')
 
 class TwitchModerationAPI {
+    #headers
     constructor(authToken, clientId) {
         this.headers = {
             'Authorization': `Bearer ${authToken}`,
@@ -15,121 +15,97 @@ class TwitchModerationAPI {
         };
     }
 
-    async requestTwitchApi(options, jsonOpt) {
-        if (jsonOpt === true) {
-            return new Promise((resolve, reject) => {
-                request(options, (error, response, body) => {
-                    const data = JSON.parse(body);
-                    if (error) return reject(error);
-                    resolve(data);
-                });
-            });
-        } else {
-            request(options, (error, response, body) => {
-                return console.log(body);
-            });
-        }
-    }
-
     async GetInfoUser(...nomeUserTwitch) {
         try {
-            const promises = nomeUserTwitch.map(nomeUser => {
-                const options = {
-                    url: `https://api.twitch.tv/helix/users?login=${nomeUser}`,
-                    method: GET,
-                    headers: this.headers
-                };
-
-                return new Promise((resolve, reject) => {
-                    request(options, (error, response, body) => {
-                        if (error) {
-                            return reject(error);
-                        }
-                        const data = JSON.parse(body).data;
-                        const userId = data.find(user => user.login === nomeUser);
-                        resolve(userId ? userId.id : null);
+            const userIdPromises = nomeUserTwitch.map(async (nome) => {
+                try {
+                    const result = await axios.get(`https://api.twitch.tv/helix/users?login=${nome}`, {
+                        headers: this.headers,
                     });
-                });
+
+                    const userId = result.data.data.find((user) => user.login === nome);
+                    return userId !== null ? userId.id : null;
+                } catch (err) {
+                    throw err;
+                }
             });
 
-            const usersIds = await Promise.all(promises);
-            return usersIds;
+            return await Promise.all(userIdPromises);
         } catch (error) {
             console.log(`Error: ${error}`);
         }
     }
 
     async BanUser(channel, moderatorBot, userBanned, motivoBan = 'Nada') {
-        const [resolvedChannel, resolvedModeratorBot, resolvedUserBanned] = await this.GetInfoUser(channel, moderatorBot, userBanned);
+        try {
+            const [resolvedChannel, resolvedModeratorBot, resolvedUserBanned] = await this.GetInfoUser(channel, moderatorBot, userBanned);
 
-        const options = {
-            url: `https://api.twitch.tv/helix/moderation/bans?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`,
-            method: POST,
-            headers: this.headers,
-            body: JSON.stringify({
+            const result = await axios.post(`https://api.twitch.tv/helix/moderation/bans?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`, {
                 data: {
-                    'user_id': `${resolvedUserBanned}`,
-                    'reason': `${motivoBan}`
+                    user_id: resolvedUserBanned,
+                    reason: motivoBan
                 }
+            }, {
+                headers: this.headers
             })
-        };
 
-        await this.requestTwitchApi(options, false);
+            return result.data.data[0]
+        } catch (error) {
+            return error.response.data.message
+        }
     }
 
     async unBanUser(channel, moderatorBot, userBanned) {
-        const [resolvedChannel, resolvedModeratorBot, resolvedUserBanned] = await this.GetInfoUser(channel, moderatorBot, userBanned);
+        try {
+            const [resolvedChannel, resolvedModeratorBot, resolvedUserBanned] = await this.GetInfoUser(channel, moderatorBot, userBanned);
 
-        const options = {
-            url: `https://api.twitch.tv/helix/moderation/bans?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}&user_id=${resolvedUserBanned}`,
-            method: DELETE,
-            headers: this.headers
-        };
+            const result = await axios.delete(`https://api.twitch.tv/helix/moderation/bans?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}&user_id=${resolvedUserBanned}`, {
+                headers: this.headers
+            })
 
-        await this.requestTwitchApi(options, false);
+            return `User unbanned.`
+        } catch (error) {
+            return error.response.data.message
+        }
     }
 
-    async GetBlockedTerms(channel, moderatorBot, full = true) {
+    async GetBlockedTerms(channel, moderatorBot, search = true) {
         try {
             const [resolvedChannel, resolvedModeratorBot] = await this.GetInfoUser(channel, moderatorBot);
 
-            const options = {
-                url: `https://api.twitch.tv/helix/moderation/blocked_terms?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`,
-                method: GET,
+            const result = await axios.get(`https://api.twitch.tv/helix/moderation/blocked_terms?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`, {
                 headers: this.headers
-            };
+            })
 
-            const responseData = await this.requestTwitchApi(options, true);
-
-            if (full === true) {
-                return responseData;
+            if (search === true) {
+                return result.data.data
             }
 
-            const verificPalavraBlock = responseData.data.find(e => e.text === full);
-            if (!verificPalavraBlock) {
-                return `Palavra ${full} não encontrada!`;
+            const verificFraseBlock = result.data.data.find(t => t.text === search)
+            if (!verificFraseBlock) {
+                return `Word not found.`
             }
 
-            return verificPalavraBlock.id;
+            return verificFraseBlock.id
         } catch (error) {
-            return error;
+            return error.response.data
         }
     }
 
     async AddBlockedTerm(channel, moderatorBot, textBlock) {
-        const [resolvedChannel, resolvedModeratorBot] = await this.GetInfoUser(channel, moderatorBot);
+        try {
+            const [resolvedChannel, resolvedModeratorBot] = await this.GetInfoUser(channel, moderatorBot);
 
-        const options = {
-            url: `https://api.twitch.tv/helix/moderation/blocked_terms?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`,
-            method: POST,
-            headers: this.headers,
-            body: JSON.stringify({
-                'text': `${textBlock}`
+            const result = await axios.post(`https://api.twitch.tv/helix/moderation/blocked_terms?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`, {
+                text: textBlock
+            }, {
+                headers: this.headers
             })
-        };
 
-        await this.requestTwitchApi(options, true);
-        console.log(`Palavra ${textBlock} bloqueada com sucesso!`);
+            return result.data.data[0]
+        } catch (error) {
+            return error.response.data.message
+        }
     }
 
     async RemoveBlockedTerm(channel, moderatorBot, ...messageBlocks) {
@@ -139,70 +115,71 @@ class TwitchModerationAPI {
             for (const messageBlock of messageBlocks) {
                 const idMessageBlock = await this.GetBlockedTerms(channel, moderatorBot, messageBlock);
 
-                const options = {
-                    url: `https://api.twitch.tv/helix/moderation/blocked_terms?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}&id=${idMessageBlock}`,
-                    method: DELETE,
+                await axios.delete(`https://api.twitch.tv/helix/moderation/blocked_terms?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}&id=${idMessageBlock}`, {
                     headers: this.headers
-                };
-
-                await this.requestTwitchApi(options, false);
+                })
             }
+            return `Text [${messageBlocks.join(', ')}] deleted.`
         } catch (error) {
-            console.log(`Error: ${error}`);
+            return error.response.data
         }
     }
 
     async DeleteChatAllMessages(channel, moderatorBot) {
-        const [resolvedChannel, resolvedModeratorBot] = await this.GetInfoUser(channel, moderatorBot);
+        try {
+            const [resolvedChannel, resolvedModeratorBot] = await this.GetInfoUser(channel, moderatorBot);
 
-        const options = {
-            url: `https://api.twitch.tv/helix/moderation/chat?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`,
-            method: DELETE,
-            headers: this.headers
-        };
+            await axios.delete(`https://api.twitch.tv/helix/moderation/chat?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`, {
+                headers: this.headers
+            })
 
-        await this.requestTwitchApi(options, false);
-        console.log('Todas as mensagens do chat foram apagadas!');
+            return `All chat messages have been deleted.`
+        } catch (error) {
+            return error.response.data
+        }
     }
 
     async UpdateChatSettings(channel, moderatorBot, chatSettings = {}) {
-        const [resolvedChannel, resolvedModeratorBot] = await this.GetInfoUser(channel, moderatorBot);
+        try {
+            const [resolvedChannel, resolvedModeratorBot] = await this.GetInfoUser(channel, moderatorBot);
 
-        const finalChatSettings = {
-            slow_mode: true,
-            slow_mode_wait_time: 3,
-            follower_mode: false,
-            follower_mode_duration: null,
-            subscriber_mode: false,
-            emote_mode: false,
-            unique_chat_mode: false,
-            non_moderator_chat_delay: false,
-            non_moderator_chat_delay_duration: null,
-            ...chatSettings
-        };
+            const finalChatSettings = {
+                slow_mode: true,
+                slow_mode_wait_time: 10,
+                follower_mode: false,
+                follower_mode_duration: null,
+                subscriber_mode: false,
+                emote_mode: false,
+                unique_chat_mode: false,
+                non_moderator_chat_delay: false,
+                non_moderator_chat_delay_duration: null,
+                ...chatSettings
+            };
 
-        const options = {
-            url: `https://api.twitch.tv/helix/chat/settings?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`,
-            method: PATCH,
-            headers: this.headers,
-            body: JSON.stringify(finalChatSettings)
-        };
+            const result = await axios.patch(`https://api.twitch.tv/helix/chat/settings?broadcaster_id=${resolvedChannel}&moderator_id=${resolvedModeratorBot}`, finalChatSettings, {
+                headers: this.headers
+            });
 
-        await this.requestTwitchApi(options);
+            return result.data.data;
+        } catch (error) {
+            return error.response.data;
+        }
     }
 
     async SendWhisper(userModBot, userMSG, Msg) {
-        const [bot, user] = await this.GetInfoUser(userModBot, userMSG)
-        const options = {
-            url: `https://api.twitch.tv/helix/whispers?from_user_id=${bot}&to_user_id=${user}`,
-            method: POST,
-            headers: this.headers,
-            body: JSON.stringify({
-                'message': `${Msg}`
-            })
-        }
+        try {
+            const [bot, user] = await this.GetInfoUser(userModBot, userMSG)
 
-        await this.requestTwitchApi(options).then(() => console.log(`Mensagem enviada com sucesso!`))
+            const result = await axios.post(`https://api.twitch.tv/helix/whispers?from_user_id=${bot}&to_user_id=${user}`, {
+                message: Msg
+            }, {
+                headers: this.headers
+            })
+
+            return `Message sent to the user ${userMSG}`
+        } catch (error) {
+            return error.response.data
+        }
     }
 }
 
